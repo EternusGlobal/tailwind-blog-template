450e859fa82044d054bf141726410e8e14623f2c
KristopherN <kris.eternus@gmail.com>
2021-01-28 14:50:32 +0800

    Update blog template

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-const switchBtn = document.querySelector('.darkmode');
+// const switchBtn = document.querySelector('.darkmode');
 const htmlTag = document.querySelector('html');
-const text = document.querySelector('.theme-label');
+// const text = document.querySelector('.theme-label');
 const scrollBtn = document.querySelector('.scroll-to-top');
 const menu = document.querySelector('.hamburger');
 const links = document.querySelector('.nav-menu');
 const menuIcon = document.querySelector('.hamburger > i');
+const mode = document.querySelector('.switch-mode');
+const themeIcon = document.querySelector('.switch-mode > i');
 
 // Menu
 
@@ -17,23 +19,37 @@ const toggleMenu = () => {
   }
 };
 
-const toggleSwitch = () => {
-  if (switchBtn.checked) {
+let isActive = false;
+
+const toggleTheme = () => {
+  if (!isActive) {
     darkMode();
+    isActive = true;
+    themeIcon.classList.replace('fa-sun', 'fa-moon');
   } else {
     lightMode();
+    isActive = false;
+    themeIcon.classList.replace('fa-moon', 'fa-sun');
   }
 };
 
+// const toggleSwitch = () => {
+//   if (switchBtn.checked) {
+//     darkMode();
+//   } else {
+//     lightMode();
+//   }
+// };
+
 const lightMode = () => {
   htmlTag.classList.remove('dark');
-  text.innerText = 'Light Mode';
+  // text.innerText = 'Light Mode';
   localStorage.setItem('theme', 'light');
 };
 
 const darkMode = () => {
   htmlTag.classList.add('dark');
-  text.innerText = 'Dark Mode';
+  // text.innerText = 'Dark Mode';
   localStorage.setItem('theme', 'dark');
 };
 
@@ -41,18 +57,21 @@ const scrollToTop = () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 };
 
-switchBtn.addEventListener('change', toggleSwitch);
+// switchBtn.addEventListener('change', toggleSwitch);
 scrollBtn.addEventListener('click', scrollToTop);
 menu.addEventListener('click', toggleMenu);
+mode.addEventListener('click', toggleTheme);
 
 window.addEventListener('DOMContentLoaded', () => {
   if (localStorage.theme === 'dark') {
     htmlTag.classList.add('dark');
-    text.innerText = 'Dark Mode';
-    switchBtn.checked = true;
+    // text.innerText = 'Dark Mode';
+    // switchBtn.checked = true;
+    themeIcon.classList.replace('fa-sun', 'fa-moon');
   } else {
     htmlTag.classList.remove('dark');
-    text.innerText = 'Light Mode';
+    // text.innerText = 'Light Mode';
+    themeIcon.classList.replace('fa-moon', 'fa-sun');
   }
 });
 
